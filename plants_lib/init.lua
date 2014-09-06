a0d2fca05b658f42740f34ae32d4ea69deca1549
HybridDog <adresse_test@t-online.de>
2014-09-06 15:23:15 +0200

    disable debug

--- plants_lib/init.lua
+++ plants_lib/init.lua
@@ -23,7 +23,7 @@ else
     S = function ( s ) return s end
 end
 
-local DEBUG = 1--false --... except if you want to spam the console with debugging info :-)
+local DEBUG = false --... except if you want to spam the console with debugging info :-)
 
 function plantslib:dbg(msg)
 	if DEBUG then
